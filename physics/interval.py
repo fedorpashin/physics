1e1fb3a62377333f9f24e2176066c7213bfafc35
Fedor Pashin <fedorpashin@icloud.com>
2021-09-25 18:34:06 +0300

    #12 Replace __len__ method with length in Interval

--- physics/interval.py
+++ physics/interval.py
@@ -1,3 +1,5 @@
+from physics.length import HasLength
+
 from dataclasses import dataclass
 from typing import Union
 
@@ -5,12 +7,13 @@ __all__ = ['Interval']
 
 
 @dataclass(frozen=True)
-class Interval:
+class Interval(HasLength):
     l: Union[float, int]
     r: Union[float, int]
 
     def __post_init__(self):
         assert self.l < self.r
 
-    def __len__(self):
+    @property
+    def length(self) -> float:
         return self.r - self.l
