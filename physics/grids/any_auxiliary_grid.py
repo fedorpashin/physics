94aaa4f2fc5c6aefabe4b372f21ba666b4f17cce
Fedor Pashin <fedorpashin@icloud.com>
2021-09-29 15:35:41 +0300

    #24 Fix the base class of AnyAuxiliaryGrid

--- physics/grids/any_auxiliary_grid.py
+++ physics/grids/any_auxiliary_grid.py
@@ -1,9 +1,11 @@
-from abc import ABC, abstractmethod
+from physics.grids.any_grid import AnyGrid
+
+from abc import abstractmethod
 
 __all__ = ['AnyAuxiliaryGrid']
 
 
-class AnyAuxiliaryGrid(ABC):
+class AnyAuxiliaryGrid(AnyGrid):
     @abstractmethod
     def point_with_half(self, i: int) -> float:
         pass
